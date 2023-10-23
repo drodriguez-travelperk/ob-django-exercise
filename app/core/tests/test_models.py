10711f51b455c465435e79ed8d349b5b2a328cfc
Diego Rodriguez <diego.rodriguez@MacBook-Pro-91.local>
2023-10-23 17:12:24 +0200

    Created recipe module, added database model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,17 @@
+"""
+Tests for database models
+"""
+from django.test import TestCase
+
+from core import models
+
+class ModelTests(TestCase):
+    """Test models"""
+
+    def test_create_recipe(self):
+        recipe = models.Recipe.objects.create(
+            name="Barbecue",
+            description="Best food in the world."
+        )
+
+        self.assertEqual(str(recipe), recipe.name)
